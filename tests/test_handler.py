10e43972a48ea0f4ceea4811755865fa736bd0e1
Eray Erdin <eraygezer.94@gmail.com>
2019-05-20 03:19:58 +0300

    added sending stacktrace

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,4 +1,5 @@
 import importlib.util
+import re
 
 import pytest
 import requests
@@ -9,7 +10,31 @@ class TestTelegramHandler:
         importlib.util.find_spec("django") is not None,
         reason="Will be covered in test_django.",
     )
-    def test_emit_return_type(self, telegram_handler, log_record_factory):
+    def test_emit_return_type(
+        self,
+        requests_mock,
+        read_test_resource,
+        telegram_handler,
+        log_record_factory,
+    ):
+        send_message_rule = re.compile(
+            "https:\/\/api.telegram.org\/bot.+\/sendMessage"
+        )
+        send_document_rule = re.compile(
+            "https:\/\/api.telegram.org\/bot.+\/sendDocument"
+        )
+
+        requests_mock.register_uri(
+            "POST",
+            send_message_rule,
+            text=read_test_resource("message.response.json").read(),
+        )
+        requests_mock.register_uri(
+            "POST",
+            send_document_rule,
+            text=read_test_resource("message.response.json").read(),
+        )
+
         log_record = log_record_factory()
 
         responses = telegram_handler.emit(log_record)
