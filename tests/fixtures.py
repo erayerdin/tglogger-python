ffd2ab0720a46eb35c54e3a8d814381bcab0799a
Eray Erdin <eraygezer.94@gmail.com>
2019-05-21 15:05:02 +0300

    updated log record factory fixture

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -30,7 +30,8 @@ def log_record_factory(
     logger: logging.Logger
 ) -> typing.Callable[[], logging.LogRecord]:
     def factory(**kwargs) -> logging.LogRecord:
-        return logging.LogRecord(
+        internal_factory = logging.getLogRecordFactory()
+        return internal_factory(
             name=kwargs.get("name", logger.name),
             level=kwargs.get("level", logging.ERROR),
             pathname=kwargs.get("pathname", "/foo/bar/baz.py"),
