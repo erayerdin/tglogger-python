cb18b6beff6f20d4850277682b96230b97d6ccbd
Eray Erdin <eraygezer.94@gmail.com>
2019-05-23 20:27:06 +0300

    removed redundant imports on fixtures

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -1,14 +1,3 @@
-import json
-import logging
-import typing
-
-import pytest
-import requests_mock
-
-import tglogger.formatter
-import tglogger.handler
-import tglogger.request
-
 from .internal import *
 from .logging import *
 from .resource import *
