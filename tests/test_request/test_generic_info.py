4bd374e015195d1a6e0563259d6fad85dcb43bfa
Eray Erdin <eraygezer.94@gmail.com>
2019-05-24 00:08:48 +0300

    unwrapped generic info tests

--- tests/test_request/test_generic_info.py
+++ tests/test_request/test_generic_info.py
@@ -1,12 +1,13 @@
 import pytest
 
 
-class TestSendLogGenericInfoRequest:
-    def test_body_chat_id(self, generic_info_request_body):
-        assert generic_info_request_body["chat_id"][0] == "1"
+def test_request_body_chat_id(generic_info_request_body):
+    assert generic_info_request_body["chat_id"][0] == "1"
 
-    def test_body_parse_mode(self, generic_info_request_body):
-        assert generic_info_request_body["parse_mode"][0] == "markdown"
 
-    def test_body_text(self, generic_info_request_body):
-        assert "text" in generic_info_request_body
+def test_request_body_parse_mode(generic_info_request_body):
+    assert generic_info_request_body["parse_mode"][0] == "markdown"
+
+
+def test_request_body_text(generic_info_request_body):
+    assert "text" in generic_info_request_body
