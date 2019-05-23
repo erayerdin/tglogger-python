b503d89c7bb61db94a4cf3eab6e0dcb3d281d76e
Eray Erdin <eraygezer.94@gmail.com>
2019-05-23 23:50:06 +0300

    unwrapped send log return type tests

--- tests/test_request.py
+++ tests/test_request.py
@@ -32,32 +32,38 @@ def send_log_responses(
     return tglogger.request.send_log(telegram_handler, exception_log_record, 1)
 
 
-class TestSendLogReturn:
-    def test_isinstance(self, send_log_responses):
-        assert isinstance(send_log_responses, dict)
+# send log return tests
+def test_isinstance(send_log_responses):
+    assert isinstance(send_log_responses, dict)
 
-    def test_length(self, send_log_responses):
-        assert len(send_log_responses) == 3
 
-    def test_key_isinstance(self, send_log_responses):
-        for key in send_log_responses:
-            assert isinstance(key, str)
+def test_length(send_log_responses):
+    assert len(send_log_responses) == 3
 
-    def test_value_isinstance(self, send_log_responses):
-        for _, value in send_log_responses.items():
-            assert (
-                isinstance(value, tglogger.request.requests.Response)
-                or value is None
-            )
 
-    def test_generic_info_response_key_name(self, send_log_responses):
-        assert "generic_info_response" in send_log_responses
+def test_key_isinstance(send_log_responses):
+    for key in send_log_responses:
+        assert isinstance(key, str)
 
-    def test_stack_trace_response_key_name(self, send_log_responses):
-        assert "stack_trace_response" in send_log_responses
 
-    def test_django_settings_response_key_name(self, send_log_responses):
-        assert "django_settings_response" in send_log_responses
+def test_value_isinstance(send_log_responses):
+    for _, value in send_log_responses.items():
+        assert (
+            isinstance(value, tglogger.request.requests.Response)
+            or value is None
+        )
+
+
+def test_generic_info_response_key_name(send_log_responses):
+    assert "generic_info_response" in send_log_responses
+
+
+def test_stack_trace_response_key_name(send_log_responses):
+    assert "stack_trace_response" in send_log_responses
+
+
+def test_django_settings_response_key_name(send_log_responses):
+    assert "django_settings_response" in send_log_responses
 
 
 @pytest.fixture
