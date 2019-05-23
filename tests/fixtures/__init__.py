2f0791382864dc1d2a491e55d076dd2f292f927a
Eray Erdin <eraygezer.94@gmail.com>
2019-05-23 20:24:55 +0300

    moved logging fixtures

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -9,48 +9,8 @@ import tglogger.formatter
 import tglogger.handler
 import tglogger.request
 
+from .logging import *
 from .resource import *
-
-
-@pytest.fixture(scope="module")
-def logger() -> logging.Logger:
-    return logging.getLogger("example.logger")
-
-
-@pytest.fixture(scope="module")
-def log_record_factory(
-    logger: logging.Logger
-) -> typing.Callable[[], logging.LogRecord]:
-    def factory(**kwargs) -> logging.LogRecord:
-        internal_factory = logging.getLogRecordFactory()
-        return internal_factory(
-            name=kwargs.get("name", logger.name),
-            level=kwargs.get("level", logging.ERROR),
-            pathname=kwargs.get("pathname", "/foo/bar/baz.py"),
-            lineno=kwargs.get("lineno", 1),
-            msg=kwargs.get("msg", "An error occured."),
-            args=kwargs.get("args", ()),
-            exc_info=kwargs.get("exc_info", None),
-            func=kwargs.get("func", "foo_bar_baz"),
-            sinfo="An error occured in whatever.",
-        )
-
-    return factory
-
-
-@pytest.fixture(scope="module")
-def exception_log_record(log_record_factory):
-    from collections import namedtuple
-
-    return log_record_factory(
-        exc_info=(
-            Exception,
-            Exception("foo"),
-            namedtuple(
-                "fake_tb", ("tb_frame", "tb_lasti", "tb_lineno", "tb_next")
-            ),  # ref: https://stackoverflow.com/a/49561567/2926992
-        )
-    )
 
 
 @pytest.fixture(scope="module")
---
+++ tests/fixtures/logging.py
@@ -0,0 +1,43 @@
+import logging, pytest
+import typing
+
+
+@pytest.fixture(scope="module")
+def logger() -> logging.Logger:
+    return logging.getLogger("example.logger")
+
+
+@pytest.fixture(scope="module")
+def log_record_factory(
+    logger: logging.Logger
+) -> typing.Callable[[], logging.LogRecord]:
+    def factory(**kwargs) -> logging.LogRecord:
+        internal_factory = logging.getLogRecordFactory()
+        return internal_factory(
+            name=kwargs.get("name", logger.name),
+            level=kwargs.get("level", logging.ERROR),
+            pathname=kwargs.get("pathname", "/foo/bar/baz.py"),
+            lineno=kwargs.get("lineno", 1),
+            msg=kwargs.get("msg", "An error occured."),
+            args=kwargs.get("args", ()),
+            exc_info=kwargs.get("exc_info", None),
+            func=kwargs.get("func", "foo_bar_baz"),
+            sinfo="An error occured in whatever.",
+        )
+
+    return factory
+
+
+@pytest.fixture(scope="module")
+def exception_log_record(log_record_factory):
+    from collections import namedtuple
+
+    return log_record_factory(
+        exc_info=(
+            Exception,
+            Exception("foo"),
+            namedtuple(
+                "fake_tb", ("tb_frame", "tb_lasti", "tb_lineno", "tb_next")
+            ),  # ref: https://stackoverflow.com/a/49561567/2926992
+        )
+    )
