16fc9ea2eecf2a47aab0eea3647e6c3f648b6554
Eray Erdin <eraygezer.94@gmail.com>
2019-05-24 00:07:37 +0300

    unwrapped stack trace tests

--- tests/test_request/test_stack_trace.py
+++ tests/test_request/test_stack_trace.py
@@ -1,24 +1,29 @@
 import re
 
 
-class TestSendLogStackTraceRequest:
-    def test_body_chat_id(self, stack_trace_request_body):
-        assert b'name="chat_id"' in stack_trace_request_body
+def test_request_body_chat_id(stack_trace_request_body):
+    assert b'name="chat_id"' in stack_trace_request_body
 
-    def test_body_parse_mode(self, stack_trace_request_body):
-        assert b'name="parse_mode"' in stack_trace_request_body
 
-    def test_body_caption(self, stack_trace_request_body):
-        assert b'name="caption"' in stack_trace_request_body
+def test_request_body_parse_mode(stack_trace_request_body):
+    assert b'name="parse_mode"' in stack_trace_request_body
 
-    def test_body_caption_content_uuid(self, stack_trace_request_body):
-        assert re.search(b"#[a-z0-9]{32}\n", stack_trace_request_body)
 
-    def test_body_caption_content_exception(self, stack_trace_request_body):
-        assert b"Exception raised by **Exception**" in stack_trace_request_body
+def test_request_body_caption(stack_trace_request_body):
+    assert b'name="caption"' in stack_trace_request_body
 
-    def test_body_reply_to_message_id(self, stack_trace_request_body):
-        assert b'name="reply_to_message_id"' in stack_trace_request_body
 
-    def test_body_document(self, stack_trace_request_body):
-        assert b'name="document"' in stack_trace_request_body
+def test_request_body_caption_content_uuid(stack_trace_request_body):
+    assert re.search(b"#[a-z0-9]{32}\n", stack_trace_request_body)
+
+
+def test_request_body_caption_content_exception(stack_trace_request_body):
+    assert b"Exception raised by **Exception**" in stack_trace_request_body
+
+
+def test_request_body_reply_to_message_id(stack_trace_request_body):
+    assert b'name="reply_to_message_id"' in stack_trace_request_body
+
+
+def test_request_body_document(stack_trace_request_body):
+    assert b'name="document"' in stack_trace_request_body
