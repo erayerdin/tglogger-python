0a225e6d86a48a4b9d7d4a857a99e9d54c7916e9
Eray Erdin <eraygezer.94@gmail.com>
2019-05-23 23:41:59 +0300

    unwrapped tests for telegram handler

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -10,12 +10,12 @@ def test_log_record_uuid(log_record_factory):
     assert isinstance(log_record.uuid, uuid.UUID)
 
 
-class TestTelegramHandler:
-    def test_emit_return_type(self, telegram_handler, log_record_factory):
-        log_record = log_record_factory()
+# Telegram Handler Tests
+def test_handler_emit_return_type(telegram_handler, log_record_factory):
+    log_record = log_record_factory()
 
-        responses = telegram_handler.emit(log_record)
-        assert isinstance(responses, dict)
+    responses = telegram_handler.emit(log_record)
+    assert isinstance(responses, dict)
 
-        for value in responses.values():
-            assert isinstance(value, requests.Response) or value is None
+    for value in responses.values():
+        assert isinstance(value, requests.Response) or value is None
