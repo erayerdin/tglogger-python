3b2beb6bce0d226d339c45bd6029ab2944b06ddf
Eray Erdin <eraygezer.94@gmail.com>
2019-05-24 00:21:41 +0300

    unwrapped TelegramFormatter tests

--- tests/test_formatter/test_telegram_formatter.py
+++ tests/test_formatter/test_telegram_formatter.py
@@ -1,37 +1,45 @@
 import re
 
 
-class TestTelegramFormatter:
-    def test_banner_hashtag(self, formatter_message):
-        assert "#tglogger" in formatter_message
+def test_message_banner_hashtag(formatter_message):
+    assert "#tglogger" in formatter_message
 
-    def test_uuid(self, formatter_message):
-        assert re.search("#[a-z0-9]{32}\n", formatter_message)
 
-    def test_logger_name(self, formatter_message):
-        assert "*Logger Name:* example.logger" in formatter_message
+def test_message_uuid(formatter_message):
+    assert re.search("#[a-z0-9]{32}\n", formatter_message)
 
-    def test_system_date(self, formatter_message):
-        assert re.search(r"\*System Date:\* .*\n", formatter_message)
 
-    def test_level(self, formatter_message):
-        assert "*Level:* #error" in formatter_message
+def test_message_logger_name(formatter_message):
+    assert "*Logger Name:* example.logger" in formatter_message
 
-    def test_path_and_line(self, formatter_message):
-        assert "*Path and Line:* _/foo/bar/baz.py_:1" in formatter_message
 
-    def test_function_or_method(self, formatter_message):
-        assert "*Function/Method:* _foo\_bar\_baz_" in formatter_message
+def test_message_system_date(formatter_message):
+    assert re.search(r"\*System Date:\* .*\n", formatter_message)
 
-    def test_thread(self, formatter_message):
-        assert re.search(
-            "\*Thread ID and Name:\* \\\[[0-9]+\] .*\n", formatter_message
-        )
 
-    def test_process(self, formatter_message):
-        assert re.search(
-            "\*Process ID and Name:\* \\\[[0-9]+\] .*\n", formatter_message
-        )
+def test_message_level(formatter_message):
+    assert "*Level:* #error" in formatter_message
 
-    def test_message(self, formatter_message):
-        assert "*Message*\n" in formatter_message
+
+def test_message_path_and_line(formatter_message):
+    assert "*Path and Line:* _/foo/bar/baz.py_:1" in formatter_message
+
+
+def test_message_function_or_method(formatter_message):
+    assert "*Function/Method:* _foo\_bar\_baz_" in formatter_message
+
+
+def test_message_thread(formatter_message):
+    assert re.search(
+        "\*Thread ID and Name:\* \\\[[0-9]+\] .*\n", formatter_message
+    )
+
+
+def test_message_process(formatter_message):
+    assert re.search(
+        "\*Process ID and Name:\* \\\[[0-9]+\] .*\n", formatter_message
+    )
+
+
+def test_message_message(formatter_message):
+    assert "*Message*\n" in formatter_message
